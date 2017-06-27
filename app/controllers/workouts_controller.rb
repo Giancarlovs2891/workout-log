55a4f844575b8eaf9bf1d919453a6903f04cbd9b
giancarlovs2891 <giancarlo.valencia@saludtec.co>
2017-06-27 16:22:31 -0500

    Add workout controller

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,30 @@
+class WorkoutsController < ApplicationController
+
+  def index
+  end
+
+  def show
+    #code
+  end
+
+  def new
+    #code
+  end
+
+  def create
+    #code
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
+
+end
